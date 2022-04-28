a3ea789f14eeb03effd018a1af7f158cc312d109
YaodongLiu <ttttttliu@qq.com>
2022-04-28 17:49:26 +0800

    Powermock-Argument Matcher

---
+++ src/main/java/com/example/powermockdemo/dao/dos/impl/UserDaoArgumentMatch.java
@@ -0,0 +1,14 @@
+package com.example.powermockdemo.dao.dos.impl;
+
+/**
+ * class classname
+ *
+ * @author : liuyaodong
+ * @date 2022/4/28
+ */
+public class UserDaoArgumentMatch {
+
+    public String getPhoneNumber(String userName){
+        throw new UnsupportedOperationException();
+    }
+}
